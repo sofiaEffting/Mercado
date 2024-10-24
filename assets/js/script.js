91335e6fd3a219439d7d3d07596146c3c25285b8
efftingsofia@gmail.com <efftingsofia@gmail.com>
2024-10-24 19:25:35 -0300

    Sla

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,29 @@
 let numero_jogadores = 0
-/*
+
 document.body.onload = () => {
     document.getElementById("container-regras").style.display = "none";
     document.getElementById("container-formulario").style.display = "flex";
     document.getElementById("jogo").style.display = "none";
-}*/
+}
 
 function mostrar_regras() {
     document.getElementById("jogo").style.display = "none";
-    document.getElementById("container-regras").style.display = "flex";
     document.getElementById("container-formulario").style.display = "none";
+    document.getElementById("container-regras").style.display = "flex";
 }
 
-function jogar() {
+document.getElementById("botao-regras-sair").addEventListener("click", () => {
     document.getElementById("container-regras").style.display = "none";
     document.getElementById("jogo").style.display = "flex";
-}
+})
 
-document.getElementById("botao-enviar").addEventListener("click", () => {
+document.getElementById("botao-enviar").addEventListener("click", function(event) {
+    event.preventDefault();  // Previna o comportamento padrão do formulário
     numero_jogadores = document.getElementById("usuarios").value;
     if (validar_numero_jogadores()) {
         mostrar_regras();
     }
-})
+});
 
 document.getElementById("icone_info").addEventListener("click", () => {
     mostrar_regras();
@@ -41,10 +42,7 @@ function validar_numero_jogadores() {
 }
 
 function numero_cartelas_por_jogador() {
-    if (numero_jogadores <= 1 || numero_jogadores > 4) {
-        alert("Número de jogadores inválido. Tente novamente.")
-        return 0
-    }
+    validar_numero_jogadores();
     return Math.ceil(4 / numero_jogadores);
 }
 
